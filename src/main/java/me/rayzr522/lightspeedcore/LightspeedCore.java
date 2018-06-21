eb5946a2ac536602fbd2dad756dff9caf7a633cb
Rayzr522 <rayzr522@gmail.com>
2018-06-21 13:21:44 -0400

    Added base project setup

---
+++ src/main/java/me/rayzr522/lightspeedcore/LightspeedCore.java
@@ -0,0 +1,11 @@
+package me.rayzr522.lightspeedcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LightspeedCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // TODO: well, this is empty :p
+    }
+}
